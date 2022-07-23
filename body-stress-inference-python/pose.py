51c4e838476d964c09b2f77a0542c6f7682940e4
RoyAtaya <roy.ataya@gmail.com>
2022-07-23 16:34:44 -0700

    updated reba angle display

--- body-stress-inference-python/pose.py
+++ body-stress-inference-python/pose.py
@@ -13,8 +13,8 @@ cap = cv2.VideoCapture(0)
 def determining_joints():
     PORT  = 1755
     socketIsOpen = False
-    # Curl counter variables
-    counter = 0 
+    # Curl reba_angle variables
+    reba_angle = 0 
     stage = None
     SAMPLE_SIZE = 10
     angleArr = []
@@ -65,7 +65,7 @@ def determining_joints():
                 else:
                     avgAngle = sum(angleArr) / len(angleArr)
                     rebaLeftArm = rebaAnalysis.CalcUpperArmPosREBA(nose[0] - left_ear[0], left_body_angle) # do REBA analysis taken on angle
-                    counter = rebaLeftArm # this needs to be removed
+                    reba_angle = rebaLeftArm # this needs to be removed
                     print(rebaLeftArm)
                     angleArr = []
                     
@@ -88,22 +88,17 @@ def determining_joints():
                 cv2.putText(image, str(left_body_angle), tuple(np.multiply(left_elbow, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
                 cv2.putText(image, str(right_body_angle), tuple(np.multiply(right_elbow, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
                 
-                # Curl counter logic
+                # Curl reba_angle logic
                 # if angle > 160:
                 #     stage = "down"
                 # if angle < 30 and stage =='down':
                 #     stage="up"
-                #     counter +=1
+                #     reba_angle +=1
             except:
                 pass    
             
-            # Render curl counter
-            # Setup status box
-            cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
-            
-            # # Rep data
-            # cv2.putText(image, 'REPS', (15,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-            cv2.putText(image, str(counter), (10,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            # Render Reba angle
+            cv2.putText(image, 'REBA Angle:' + str(reba_angle), (0,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
             
             # # Stage data
             # cv2.putText(image, 'STAGE', (65,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
