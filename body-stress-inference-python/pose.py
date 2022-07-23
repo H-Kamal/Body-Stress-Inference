910d300887dd5c280f4bd4963615791a1e374222
RoyAtaya <roy.ataya@gmail.com>
2022-07-22 20:46:06 -0700

    fix

--- body-stress-inference-python/pose.py
+++ body-stress-inference-python/pose.py
@@ -13,6 +13,7 @@ def determining_joints():
     # Curl counter variables
     counter = 0 
     stage = None
+    
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
         while cap.isOpened():
             ret, frame = cap.read()
@@ -42,11 +43,12 @@ def determining_joints():
                 right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
                 right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
                 right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-
+                
                 # Calculate angle
                 left_body_angle = calculate_angle(left_hip, left_shoulder, left_elbow)
-                right_body_angle = calculate_angle(right_hip, right_shoulder, right_elbow)
-                
+                # right_body_angle = calculate_angle(right_hip, right_shoulder, right_elbow)
+                right_body_angle = -999
+                # Check threshold before adding body parts data to body parts dictionary and sending data.
                 body_parts = {
                     "Left Shoulder": left_shoulder,
                     "Left Elbow" : left_elbow,
@@ -78,15 +80,15 @@ def determining_joints():
             
             # Render curl counter
             # Setup status box
-            cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+            # cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
             
-            # Rep data
-            cv2.putText(image, 'REPS', (15,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-            cv2.putText(image, str(counter), (10,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            # # Rep data
+            # cv2.putText(image, 'REPS', (15,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            # cv2.putText(image, str(counter), (10,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
             
-            # Stage data
-            cv2.putText(image, 'STAGE', (65,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-            cv2.putText(image, stage, (60,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            # # Stage data
+            # cv2.putText(image, 'STAGE', (65,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            # cv2.putText(image, stage, (60,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
             
             # Render detections
             mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
@@ -114,5 +116,5 @@ def calculate_angle(a, b, c):
     
     if angle > 180.0:
         angle = 360-angle
-        
+    
     return angle 
