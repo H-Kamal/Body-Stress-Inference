69cc480c467bdb4fb98e097117fef70d0fbaf47a
RoyAtaya <roy.ataya@gmail.com>
2022-07-23 18:06:17 -0700

    removed commented lines in server.py

--- body-stress-inference-python/server.py
+++ body-stress-inference-python/server.py
@@ -1,8 +1,6 @@
 import socket
-# import random
 import json
 from time import sleep
-# import pose
 
 PORT = 1755
 
@@ -10,22 +8,10 @@ def connectSocket(PORT):
     hostname = socket.gethostname()
     IPAddr = socket.gethostbyname(hostname)
     
-    # print("Your Computer Name is:" + hostname)
-    # print("Your Computer IP Address is:" + IPAddr)
-    
     s = socket.socket()
     s.connect((IPAddr, PORT))
     return s
 
-# def sendRandomColors(s):
-#     dic = pose.determining_joints()
-#     print(dic)
-#     jsonResult = json.dumps(dic)
-#     # print(jsonResult)
-    
-#     s.send(jsonResult.encode())
-#     s.close()
-    
 def sendJSONDataToUnity(s, dic):
     jsonResult = json.dumps(dic)
     
@@ -33,12 +19,6 @@ def sendJSONDataToUnity(s, dic):
     s.close()
 
 # Alone will connect to a socket provided
-if __name__ == '__main__':
-    # hostname = socket.gethostname()
-    # IPAddr = socket.gethostbyname(hostname)
-    # print("Your Computer Name is:" + hostname)
-    # print("Your Computer IP Address is:" + IPAddr)
-    
+if __name__ == '__main__':   
     s = connectSocket(PORT)
-    # sendRandomColors(s)
     sleep(1)
