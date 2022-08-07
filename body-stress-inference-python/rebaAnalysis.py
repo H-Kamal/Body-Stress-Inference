5877d3db7c94eb6658d4304254990b6f59c61057
kmelnikov98 <33918043+kmelnikov98@users.noreply.github.com>
2022-08-06 19:11:48 -0700

    angle update to leg adj
    changed angle again

--- body-stress-inference-python/rebaAnalysis.py
+++ body-stress-inference-python/rebaAnalysis.py
@@ -2,7 +2,6 @@
 def CalcUpperArmPosREBA(nose_to_ear_x, elbow_to_hip_x, angle):
 	if nose_to_ear_x * elbow_to_hip_x < 0: # facing away from origin.
 		angle = -angle # flip angle as facing away has you lifting arms up as negative
-	print("reba angle :", angle)
 	if angle < -20:
 		return 2
 	elif angle > -20 and angle < 20:
