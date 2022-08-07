562f3be7ca48e686cb414e030f0cd3cfd472cb94
kmelnikov98 <33918043+kmelnikov98@users.noreply.github.com>
2022-08-06 18:58:55 -0700

    Update rebaAnalysis.py
    update code with case when standing straight

--- body-stress-inference-python/rebaAnalysis.py
+++ body-stress-inference-python/rebaAnalysis.py
@@ -17,6 +17,8 @@ def CalcUpperArmPosREBA(nose_to_ear_x, elbow_to_hip_x, angle):
 	return -1 # means undefined, if an angle or nose_to_ear_dist was not provided, then ignore value
 
 def calcLegAdjustmentsREBA(angle):
+	if angle < 30:
+		return 0
 	if angle >= 30 and angle < 60:
 		return 1
 	elif angle >= 60:
