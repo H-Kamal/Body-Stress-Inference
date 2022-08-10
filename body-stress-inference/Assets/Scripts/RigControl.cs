4f7a7cf1143ede37db8ed55aa1c9b703084425fb
Hamza Kamal <hamzakamal123@hotmail.com>
2022-08-09 17:03:52 -0700

    Added left thigh and shin to rig controller

--- body-stress-inference/Assets/Scripts/RigControl.cs
+++ body-stress-inference/Assets/Scripts/RigControl.cs
@@ -13,6 +13,8 @@ public class RigControl : MonoBehaviour
     RigBone rightUpperArm;
     RigBone rightUpperLeg;
     RigBone rightLowerLeg;
+    RigBone leftUpperLeg;
+    RigBone leftLowerLeg;
     void Start()
     {
         leftUpperArm = new RigBone(humanoid, HumanBodyBones.LeftUpperArm);
@@ -20,17 +22,22 @@ public class RigControl : MonoBehaviour
         rightUpperArm = new RigBone(humanoid, HumanBodyBones.RightUpperArm);
         rightUpperLeg = new RigBone(humanoid, HumanBodyBones.RightUpperLeg);
         rightLowerLeg = new RigBone(humanoid, HumanBodyBones.RightLowerLeg);
+        leftUpperLeg = new RigBone(humanoid, HumanBodyBones.LeftUpperLeg);
+        leftLowerLeg = new RigBone(humanoid, HumanBodyBones.LeftLowerLeg);
+
     }
     void Update()
     {
         double t = Math.Sin(Time.time * Math.PI); // [-1, 1]
         double s = (t + 1) / 2;                       // [0, 1]
         double u = 1 - s / 2;                         // [0.5, 1]
+        // Rotates along the axis chosen in the last 3 parameters
         leftUpperArm.set((float)(80 * t), 1, 0, 0);
         leftLowerArm.set((float)(90 * s), 1, 0, 0);
         rightUpperArm.set((float)(90 * t), 0, 0, 1);
         rightUpperLeg.set((float)(180 * u), 1, 0, 0);
         rightLowerLeg.set((float)(90 * s), 1, 0, 0);
+        // Rotates the whole model at once. Currently set to 0 rotation along any axis
         humanoid.transform.rotation
           = Quaternion.AngleAxis(bodyRotation.z, new Vector3(0, 0, 1))
           * Quaternion.AngleAxis(bodyRotation.x, new Vector3(1, 0, 0))
