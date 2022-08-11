e995ddc4b33cb8fa30da95bd784d9ad9f11d7d2b
RoyAtaya <roy.ataya@gmail.com>
2022-08-11 15:16:41 -0700

    buttons to switch scenes work

---
+++ body-stress-inference/Assets/Scripts/SceneController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadResultScene(){
+        SceneManager.LoadScene("ResultScene");
+    }
+    public void LoadStartScene(){
+        SceneManager.LoadScene("StartScene");
+    }
+}
