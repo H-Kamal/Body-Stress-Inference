61276e369cadd1a58dd3566b7fefaedc18e30b10
Hamza Kamal <hamzakamal123@hotmail.com>
2022-08-12 17:24:04 -0700

    Added coloring to every bodypart. Still have color clipping

--- body-stress-inference/Assets/Scripts/ColorREBARisk.cs
+++ body-stress-inference/Assets/Scripts/ColorREBARisk.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class ColorREBARisk : MonoBehaviour
 {
     private GameObject upperLeftArm;
+    private GameObject lowerLeftArm;
+    private GameObject upperRightArm;
+    private GameObject lowerRightArm;
+    private GameObject leftShin;
+    private GameObject rightShin;
+    private GameObject leftThigh;
+    private GameObject rightThigh;
+    private GameObject head;
+    private GameObject torso;
     private Renderer upperLeftArmRenderer;
     // Start is called before the first frame update
     void Start()
     {
         upperLeftArm = GameObject.Find("UpperLeftArm");
+        lowerLeftArm = GameObject.Find("LowerLeftArm");
+        upperRightArm = GameObject.Find("UpperRightArm");
+        lowerRightArm = GameObject.Find("LowerRightArm");
+        leftShin = GameObject.Find("LeftShin");
+        rightShin = GameObject.Find("RightShin");
+        leftThigh = GameObject.Find("LeftThigh");
+        rightThigh = GameObject.Find("RightThigh");
+        head = GameObject.Find("Head");
+        torso = GameObject.Find("Torso");
+        
         upperLeftArmRenderer = upperLeftArm.GetComponent<Renderer>();
     }
 
@@ -18,7 +37,16 @@ public class ColorREBARisk : MonoBehaviour
     {   
         if(REBA.REBAScoreColors.Count > 0)
         {
-            upperLeftArmRenderer.material.color = REBA.REBAScoreColors["rebaUpperLeftArm"];
+            upperLeftArm.GetComponent<Renderer>().material.color  = REBA.REBAScoreColors["rebaUpperLeftArm"];
+            lowerLeftArm.GetComponent<Renderer>().material.color  = REBA.REBAScoreColors["rebaLowerLeftArm"];
+            upperRightArm.GetComponent<Renderer>().material.color = REBA.REBAScoreColors["rebaUpperRightArm"];
+            lowerRightArm.GetComponent<Renderer>().material.color = REBA.REBAScoreColors["rebaLowerRightArm"];
+            leftThigh.GetComponent<Renderer>().material.color = REBA.REBAScoreColors["rebaLegAdj"];
+            rightThigh.GetComponent<Renderer>().material.color = REBA.REBAScoreColors["rebaLegAdj"];
+            leftShin.GetComponent<Renderer>().material.color = REBA.REBAScoreColors["rebaLegAdj"];
+            rightShin.GetComponent<Renderer>().material.color = REBA.REBAScoreColors["rebaLegAdj"];
+            torso.GetComponent<Renderer>().material.color = REBA.REBAScoreColors["rebaTrunkAdj"];            
+            head.GetComponent<Renderer>().material.color = REBA.REBAScoreColors["rebaNeck"];
         }
     }
 }
